1990b359aa8f30ec44d543bc79460a2ea3338e7e
ivanvysotsky <ivan.ser2005@mail.ru>
2021-02-08 18:50:53 +0300

    fix UTF-8

--- pypaint.py
+++ pypaint.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 from tkinter import *
 
 canvas_width = 700
@@ -37,16 +39,16 @@ w = Canvas(root,
            bg="white")
 w.bind("<B1-Motion>", paint)
 
-red_btn = Button(text="Красный", width=10,
+red_btn = Button(text="РљСЂР°СЃРЅС‹Р№", width=10,
                  command=lambda: color_change("red"))
-blue_btn = Button(text="Синий", width=10,
+blue_btn = Button(text="РЎРёРЅРёР№", width=10,
                   command=lambda: color_change("blue"))
-black_btn = Button(text="Чёрный", width=10,
+black_btn = Button(text="Р§С‘СЂРЅС‹Р№", width=10,
                    command=lambda: color_change("black"))
-white_btn = Button(text="Ластик", width=10,
+white_btn = Button(text="Р›Р°СЃС‚РёРє", width=10,
                    command=lambda: color_change("white"))
 
-clear_btn = Button(text="Очистить", width=10,
+clear_btn = Button(text="РћС‡РёСЃС‚РёС‚СЊ", width=10,
                    command=lambda: w.delete("all"))
 
 five_btn = Button(text="5", width=10,
@@ -60,34 +62,34 @@ fifteen_btn = Button(text="15", width=10,
 twenty_btn = Button(text="20", width=10,
                     command=lambda: brish_size_change(20))
 
-size_lbl = Label(text="Размер кисти:", width=10)
-color_lbl = Label(text="Цвет:", width=10)
+size_lbl = Label(text="Р Р°Р·РјРµСЂ РєРёСЃС‚Рё:", width=10)
+color_lbl = Label(text="Р¦РІРµС‚:", width=10)
 
 w.grid(row=2, column=0,
        columnspan=7, padx=5,
        pady=5, sticky=E + W + S + N)
 
-#кисть -- brush
+#РєРёСЃС‚СЊ -- brush
 w.columnconfigure(6, weight=1)
 w.rowconfigure(2, weight=1)
 
-#цвета -- color
+#С†РІРµС‚Р° -- color
 red_btn.grid(row=0, column=2)
 blue_btn.grid(row=0, column=3)
 black_btn.grid(row=0, column=4)
 
-#функции -- function
+#С„СѓРЅРєС†РёРё -- function
 white_btn.grid(row=0, column=5)
 clear_btn.grid(row=0, column=6)
 
-#размер -- size
+#СЂР°Р·РјРµСЂ -- size
 ein_btn.grid(row=1, column=2)
 five_btn.grid(row=1, column=3)
 ten_btn.grid(row=1, column=4)
 fifteen_btn.grid(row=1, column=5)
 twenty_btn.grid(row=1, column=6)
 
-#надписи -- Label
+#РЅР°РґРїРёСЃРё -- Label
 size_lbl.grid(row=1, column=1)
 color_lbl.grid(row=0, column=1)
 root.mainloop()
