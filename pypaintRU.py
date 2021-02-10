79b568266633bcd3510ce0ab2180350a3aad1ce0
ivanvysotsky <ivan.ser2005@mail.ru>
2021-02-10 14:35:32 +0300

    new color, size, fix canvas, new division, v2.0(RU)(py)

--- pypaintRU.py
+++ pypaintRU.py
@@ -2,7 +2,7 @@
 
 from tkinter import *
 
-canvas_width = 900
+canvas_width = 1000
 canvas_height = 500
 brush_size = 3
 color = "black"
@@ -44,27 +44,38 @@ blue_btn = Button(text="Синий", width=10,
                   command=lambda: color_change("blue"))
 black_btn = Button(text="Чёрный", width=10,
                    command=lambda: color_change("black"))
+yellow_btn = Button(text="Жёлтый", width=10,
+                    command=lambda: color_change("yellow"))
+green_btn = Button(text="Зелёный", width=10,
+                   command=lambda: color_change("green"))
+orange_btn = Button(text="Оранжевый", width=10,
+                    command=lambda: color_change("orange"))
+
 white_btn = Button(text="Ластик", width=10,
                    command=lambda: color_change("white"))
-
 clear_btn = Button(text="Очистить", width=10,
                    command=lambda: w.delete("all"))
+pencil_btn = Button(text="Карандаш", width=10,
+                    command=lambda: brish_size_change(1))
 
 five_btn = Button(text="5", width=10,
                   command=lambda: brish_size_change(5))
-ein_btn = Button(text="1", width=10,
-                 command=lambda: brish_size_change(1))
+standart_btn = Button(text="Стандартный", width=10,
+                      command=lambda: brish_size_change(3))
 ten_btn = Button(text="10", width=10,
                  command=lambda: brish_size_change(10))
 fifteen_btn = Button(text="15", width=10,
                      command=lambda: brish_size_change(15))
 twenty_btn = Button(text="20", width=10,
                     command=lambda: brish_size_change(20))
+thirty_btn = Button(text="30", width=10,
+                    command=lambda: brish_size_change(30))
 
 size_lbl = Label(text="Размер кисти:", width=10)
 color_lbl = Label(text="Цвет:", width=10)
+fun_lbl = Label(text="Доп. функции:", width=11)
 
-w.grid(row=2, column=0,
+w.grid(row=3, column=0,
        columnspan=7, padx=5,
        pady=5, sticky=E + W + S + N)
 
@@ -73,25 +84,32 @@ w.columnconfigure(6, weight=1)
 w.rowconfigure(2, weight=1)
 
 #цвета -- color
-red_btn.grid(row=0, column=2)
-blue_btn.grid(row=0, column=3)
-black_btn.grid(row=0, column=4)
-
-#функции -- function
-white_btn.grid(row=0, column=5)
-clear_btn.grid(row=0, column=6)
+red_btn.grid(row=0, column=1)
+blue_btn.grid(row=0, column=2)
+black_btn.grid(row=0, column=3)
+yellow_btn.grid(row=0, column=4)
+green_btn.grid(row=0, column=5)
+orange_btn.grid(row=0, column=6)
 
 #размер -- size
-ein_btn.grid(row=1, column=2)
-five_btn.grid(row=1, column=3)
-ten_btn.grid(row=1, column=4)
-fifteen_btn.grid(row=1, column=5)
-twenty_btn.grid(row=1, column=6)
+standart_btn.grid(row=1, column=1)
+five_btn.grid(row=1, column=2)
+ten_btn.grid(row=1, column=3)
+fifteen_btn.grid(row=1, column=4)
+twenty_btn.grid(row=1, column=5)
+thirty_btn.grid(row=1, column=6)
+
+#функции -- function
+white_btn.grid(row=2, column=2)
+clear_btn.grid(row=2, column=3)
+pencil_btn.grid(row=2, column=1)
 
 #надписи -- Label
-size_lbl.grid(row=1, column=1)
-color_lbl.grid(row=0, column=1)
+size_lbl.grid(row=1, column=0)
+color_lbl.grid(row=0, column=0)
+fun_lbl.grid(row=2, column=0)
 
+# параметры окана -- window preferens
 root.resizable(False, False)
 root.mainloop()
 
